adf813069f9412710672d9f438ac5715ae310cc7
sca21002 <sca21002@googlemail.com>
2011-02-26 11:34:46 +0100

    Add Controller Remedi skeleton

---
+++ lib/Atacama/Controller/Remedi.pm
@@ -0,0 +1,44 @@
+package Atacama::Controller::Remedi;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+Atacama::Controller::Remedi - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched Atacama::Controller::Remedi in Remedi.');
+}
+
+
+=head1 AUTHOR
+
+Atacama Developer,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
