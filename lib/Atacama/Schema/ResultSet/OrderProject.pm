e2ab046c8bd4488bdc7515a7715af5eb19cfe3c3
sca21002 <sca21002@googlemail.com>
2011-07-13 10:19:18 +0200

    Fix deleting more than one project at once

--- lib/Atacama/Schema/ResultSet/OrderProject.pm
+++ lib/Atacama/Schema/ResultSet/OrderProject.pm
@@ -4,9 +4,6 @@ use warnings;
 
 use base qw/DBIx::Class::ResultSet/;
 
-use Data::Dumper;
-
-
 sub get_new_result_as_href {
     my $self = shift;
     my $args = shift;
@@ -53,34 +50,32 @@ sub get_projects_as_string {
 sub save {
     my $self = shift;
     my $params = shift;
-
+    
     return unless ($params and ref $params eq 'ARRAY' and @$params);
     my @primary_columns = $self->result_source->primary_columns;
     die "Funktion save nur fur einen Primärschlüssel implementiert"
         if @primary_columns > 1;
     my $primary_key = $primary_columns[0];
     my $columns_info = $self->result_source->columns_info;
-    foreach my $param (@$params) {
+    foreach my $param (@$params) {    
         my $is_empty = 1;
         foreach my $val (values %$param) {$is_empty = 0 if $val}                    
         next if $is_empty;
         my $row;
-        if (
-                exists $param->{$primary_key}
-                && defined $param->{$primary_key}
-                && $param->{$primary_key} ne ''
+        if (exists $param->{$primary_key}
+            && defined $param->{$primary_key}
+            && $param->{$primary_key} ne ''
             ) {
-            $row = $self->find( $param->{$primary_key} );
+            $row = $self->find($param->{$primary_key});
             next unless $row;
         }
-        if ( $row and $param->{DELETED} ) {
+        if ($row and $param->{DELETED}) {
             $row->delete;
-            return;
+            next;
         }
-        my %integer_type = (smallint => 1, tinyint => 1, integer => 1, mediumint => 1); 
+        my %integer_type = (smallint => 1,tinyint => 1,integer => 1,mediumint => 1); 
         next unless (%$param);
         my $column;
-        my $projectoptions;
         foreach my $key (keys %$param) {
             if (exists $columns_info->{$key}) {
                 if ($param->{$key} eq ''
@@ -88,17 +83,16 @@ sub save {
                 ){ $param->{$key} = undef; } 
                 $column->{$key} = $param->{$key};        
             }
-            elsif ($key eq 'projectoptions') {
-                $projectoptions = $param->{$key};
+            elsif ($key eq 'projectoptions' and $row) {
+                $row->save_projectoptions($param->{$key});    
             }
         }     
         if ($row) {
             $row->update($column);
         }
         else {
-            $row = $self->find_or_create($column);        # ACHTUNG!!
+            $self->create($column);        
         }
-        $row->save_projectoptions($projectoptions) if $projectoptions;
     }
 }
 
