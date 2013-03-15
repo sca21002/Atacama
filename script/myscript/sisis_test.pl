3c9b5369f8c44044edc0e2b0b3ffce48fa7529d6
sca21002 <sca21002@googlemail.com>
2013-03-15 20:34:24 +0100

    Fix LDAP test script

--- script/myscript/sisis_test.pl
+++ script/myscript/sisis_test.pl
@@ -5,6 +5,9 @@ use FindBin;
 use lib "$FindBin::Bin/../../lib";
 use Config::ZOMG;
 use UBR::Sisis::Schema;
+use Data::Dumper;
+use Carp;
+use Encode;
 
 binmode(STDOUT, ":utf8");
 
@@ -13,16 +16,31 @@ my $config = Config::ZOMG->new(
     path => File::Spec->catfile($FindBin::Bin,'..','..'),
 );
 my $config_hash = $config->load;
-my @connect = @{$config_hash->{'Model::SisisDB'}{connect_info}};  
+say Dumper($config_hash);
 
-say Dumper(@connect);
+my $connect = $config_hash->{'Model::SisisDB'}{connect_info};  
+
+say Dumper($connect);
 
 my $schema_sisis = UBR::Sisis::Schema->connect(
-    @{$config_hash->{'Model::SisisDB'}{connect_info}}  
+    @{$config_hash->{'Model::SisisDB'}{connect_info}}   
 );
 
-my $signatur = '167/ST 250 P30 D582';
+my $signatur = '999/Art.533';
 my $buch_rs
     = $schema_sisis
         ->resultset('D01buch')->search({d01ort => { 'like' => $signatur }},
 );
+
+unless ($buch_rs) {
+    carp "Kein Buch zur Signatur $signatur bekommen\n";
+    next;
+}
+
+say "Treffer: " . $buch_rs->count;
+
+while (my $buch = $buch_rs->next) {
+    say Dumper(my $r = $buch->get_titel());
+    say decode('utf8',$r->[0]{autor_avs});
+}
+
