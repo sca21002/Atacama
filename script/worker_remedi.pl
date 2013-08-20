caac5e636818aeeafd90cae585770749a4acdf60
sca21002 <sca21002@googlemail.com>
2013-08-20 10:54:36 +0200

    Make the scoreboard dir for TheSchwartz job queue user dependent

--- script/worker_remedi.pl
+++ script/worker_remedi.pl
@@ -1,11 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
+use English qw( -no_match_vars ) ;  # Avoids regex performance penalty
 use FindBin;
 use lib "$FindBin::Bin/../lib";
-use  Log::Log4perl qw(:easy);
+use Log::Log4perl qw(:easy);
 use File::Spec;
-use File::Path qw(remove_tree);
+use File::Path qw(remove_tree make_path);
 use Config::ZOMG;
 use DBI;
 use Data::Dumper;
@@ -25,36 +26,30 @@ Log::Log4perl->easy_init(
     },
 );
 
-### Konfiguration einlesen
-#my $conf = Config::Any->load_stems({
-#    stems => [ Path::Class::File->new( $FindBin::Bin, '..', 'atacama_local') ],
-#    use_ext => 1,
-#});
-#my ($filename, $config) = %{shift @$conf};
-
 my $config = Config::ZOMG->new(
     name => 'Atacama',
     path => File::Spec->catfile($FindBin::Bin, '..'),
 );
 my $config_hash = $config->load;
 
-TRACE("Konfigurationsdatei(en) " . join(', ', $config->found) . " gelesen");
+TRACE("reading configuration file" . join(', ', $config->found));
 
 ### Datenbankverbindung
 my $dbic_connect_info = $config_hash->{'Model::AtacamaDB'}{connect_info};
-LOGFATAL("Verbindungsparamter(connect_info) für die Datenbank nicht gefunden")
+LOGFATAL("No connect_info for the database found!")
     unless $dbic_connect_info;
 
 my $dbh = DBI->connect(@$dbic_connect_info);
 my $driver = Data::ObjectDriver::Driver::DBI->new( dbh => $dbh);
-LOGFATAL("Datenbank-Verbindung nicht gefunden!") unless $driver; 
+LOGFATAL("No database driver found!") unless $driver; 
 my $client = TheSchwartz->new( databases => [{ driver => $driver }],
                                verbose => 1, );
 my $current_time = $client->get_server_time($driver);
 my $dt = DateTime->from_epoch(epoch => $current_time);
 TRACE("Zeit: " . $dt->set_time_zone('Europe/Berlin')->strftime('%d.%m.%Y %T'));
-remove_tree('/tmp/theschwartz');
-$client->set_scoreboard('/tmp');
+remove_tree("/tmp/$EFFECTIVE_USER_ID");
+make_path("/tmp/$EFFECTIVE_USER_ID");
+$client->set_scoreboard("/tmp/$EFFECTIVE_USER_ID");
 INFO("Scoreboard: " . $client->scoreboard); 
 $client->can_do('Atacama::Worker::Remedi');
 $client->can_do('Atacama::Worker::Sourcefile');
