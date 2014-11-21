00e11020b734b35ccc6db7d71d414899b0115921
sca21002 <sca21002@googlemail.com>
2014-11-21 13:57:47 +0100

    Refine set_status script

--- script/myscript/set_status.pl
+++ script/myscript/set_status.pl
@@ -5,14 +5,44 @@ use Path::Tiny;
 use lib path($Bin)->parent(2)->child('lib')->stringify;
 use Atacama::Helper;
 use Modern::Perl;
+use Log::Log4perl qw(:easy);
+
+
+### intitialize easy logging
+my $logfile = path($Bin)->parent(2)->child(qw(log set_status.log));
+Log::Log4perl->easy_init(
+    { level   => $INFO,
+      file    => ">>" . $logfile,
+    },
+    { level   => $TRACE,
+        file    => 'STDOUT',
+    },
+);
+INFO('--------------------------------------------------------------------');
+INFO('Parameter: ' . join(' ',@ARGV));
+
+my ($jobname, $login) = @ARGV;
+
+LOGDIE('Jobname muss angegeben werden') unless $jobname;
+
+$login = 'anonymous' unless $login;
 
 my $schema = Atacama::Helper::get_schema(path($Bin)->parent(2));
 
 my $order = $schema->resultset('Order')->find({ 
-    order_id => 'ubr03883',
+    order_id => $jobname,
 });
 
-$order->update({status_id => 9});
+LOGDIE("Job '$jobname' nicht in der Datenbank gefunden") unless $order;
 
+my $status_id =53;
+$order->update({status_id => $status_id});
+$order->add_to_remarks({
+	status_id => $status_id, 
+	login => $login,
+	content => 'Auftrag in Scanflow angelegt'
+});
+
+INFO('Status fuer Job: ' .  $jobname . ' von ' . $login . ' geaendert');
 
 
